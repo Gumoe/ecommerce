6f5725d4c5e24c39c7c03c4c36efe304014894df
boptapdem <duyanhkakashi@gmail.com>
2024-01-11 11:51:26 +0700

    Admin

--- src/Admin/components/Admin.jsx
+++ src/Admin/components/Admin.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
 
+import { Box, List, ListItem, ListItemButton, ListItemIcon, Toolbar, useMediaQuery, useTheme} from '@mui/material';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import MailIcon from '@mui/icons-material/Mail';
+import InboxIcon from '@mui/icons-material/Inbox';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+const menu=[
+    {name: "Dashboard", path:"/admin",icon:<DashboardIcon/>},
+    {name: "Products", path:"/admin/products",icon:<DashboardIcon/>},
+    {name: "Customers", path:"/admin/customers",icon:<DashboardIcon/>},
+    {name: "Oders", path:"/admin/orders",icon:<DashboardIcon/>},
+    {name: "AddProduct", path:"/admin/product/create",icon:<DashboardIcon/>},
+    //{name: "", path:"},
+]
 const Admin = () => {
+
+    const theme=useTheme();
+    const isLargeScreen=useMediaQuery(theme.breakpoints.up("lg"));
+    const [sideBarVisiable,setSideBarVisiable]=useState(false);
+    const navigate=useNavigate();
+
+    const drawer=(
+        <Box
+        sx={{
+            overflow:"auto",
+            display:"flex",
+            flexDirection:"column",
+            justifyContent:"space-between"
+        }}
+        >
+
+            {isLargeScreen && <Toolbar/>}
+            <List>
+                {menu.map((item,index)=> <ListItem key={item.name} disablePadding onClick={()=>navigate(item.path)}>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            {item.icon}
+                        </ListItemIcon>
+                    </ListItemButton>
+                </ListItem>)}
+            </List>
+
+            <List>
+                <ListItem  disablePadding>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <AccountCircleIcon/>
+                        </ListItemIcon>
+                    </ListItemButton>
+                </ListItem>
+            </List>
+
+        </Box>
+    )
+    
   return (
-    <div>Admin</div>
+    <div>
+admin
+    </div>
   )
 }
 

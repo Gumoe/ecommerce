c8f181da3f1e0cfc2d9c5b1d7a66d4058cdf5a1e
boptapdem <duyanhkakashi@gmail.com>
2024-01-11 11:03:58 +0700

    Admin

--- src/App.js
+++ src/App.js
@@ -13,12 +13,14 @@ import { Route, Routes } from 'react-router-dom';
 import CustomerRouters from './Routers/CustomerRouters';
 import React from 'react';
 import AdminRouters from './Routers/AdminRouters';
+
+
 function App() {
   return (
     <div className="">
       <Routes>
         <Route path='/*' element={<CustomerRouters/>}></Route>
-        <Route path = '/admin/*' element = {<AdminRouters/>}></Route>
+        <Route path='/admin/*' element={<AdminRouters/>}></Route>
       </Routes>
       
     </div>
